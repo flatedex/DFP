3b14c070152a05998d993c17e3cd343bdbf21634
flatedex <flatedex@mail.ru>
2023-02-27 17:40:36 +0300

    Project created. Will be migrated in WPF soonly.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Program
+{
+    class Program
+    {
+        const double eps = 0.00001; // accuracy for method
+        double[,] d = new double[,] // initial state
+        {
+            {1,0,0},
+            {0,1,0},
+            {0,0,1}
+        };
+        public static void Main()
+        {
+
+        }
+        public double[,] Calculations(double[,] d, double eps, double x)
+        {
+            return d;
+        }
+    }
+}
